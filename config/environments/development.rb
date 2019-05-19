40800a842377de2098e717df6d9b7de243e8aa1a
khanhndo <khanhndo@kms-technology.com>
2019-05-19 14:57:02 +0700

    config memcache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
   config.cache_store = :dalli_store
+  config.action_controller.perform_caching = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
